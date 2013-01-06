856993f66e4616abc6c47a25da199383f48ea821
Lyubomir Slavilov <lyubo.slavilov@gmail.com>
2013-01-07 00:43:38 +0200

    Added Symfony/Response.php which can be used as a Symfony 2 service

---
+++ lib/Ozy/Symfony/Response.php
@@ -0,0 +1,34 @@
+<?php
+
+/*
+ * This file is part of the Ozy package.
+ * 
+ * (c) Lyubomir Slavilov <lyubo.slavilov@gmail.com>
+ * 
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+namespace Ozy\Symfony;
+use Symfony\Component\HttpFoundation\Response as SfResponse;
+/**
+ * Description of Response
+ *
+ * @author Lyubomir Slavilov <lyubo.slavilov@gmail.com>
+ */
+class Response extends SfResponse{
+	
+	static public $ozy;
+	
+	public function __construct($kernel){
+		parent::__construct();
+		
+		if(!self::$ozy){
+			self::$ozy = new \Ozy\Engine($kernel->getEnvironment());
+		}
+	}
+	
+	public function __call($method, $arguments){
+		return call_user_func(array(self::$ozy, $method), $arguments);
+	}
+	
+}
